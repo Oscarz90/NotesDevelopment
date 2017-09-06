fcc67f879b22e53df2f03461e32b6ee55f2bbf86
Oscarz90 <omsz90@live.com.mx>
2017-09-06 05:45:24 +0000

    Scripts y ejemplos de User Input

---
+++ Bash/User Input/scripts/introduction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#ask me for my name
+
+echo What is your name?
+read varname
+
+echo your name is $varname
---
+++ Bash/User Input/scripts/login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#ask the user for login details
+
+read -p 'User Name:' uservar
+read -sp 'Password:' passvar
+echo
+echo Thank you $uservar now we have your login details
